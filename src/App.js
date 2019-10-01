2f6673c1d20eb85cac9506f453be184cb89cf41e
nearjay06 <nearjoan2018@gmail.com>
2019-10-01 12:00:29 +0300

    add sidebar and navigation bar and bootsrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import images from './cat.webp';
+import Nav from './components/Navbar';
+import Side from './components/Sidebar';
+import './App.css'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <img src={images} alt=''></img>
+      <Side />
     </div>
   );
+    
+  
 }
 
 export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function Side(){
+    return(
+        <div className="menu">
+                <p >This is the sidebar.</p>
+                            
+            <div className="unordered">
+            <ul>
+                <li><a href="/">Type of Cats</a></li>
+                <li><a href="/">Prices</a></li>
+                <li><a href="/">Contact Us</a></li>
+            
+            </ul>
+            </div>
+        
+        </div>
+
+
+    );
+
+}
+
+export default Side;
